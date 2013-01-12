e36dac07707b645d557d903e48a8fb2c8fd99e4f
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-01-12 17:53:50 +0200

    Redirecto jekyll-slim to jekyll_slim instead

--- lib/jakyll_slim.rb
+++ lib/jakyll_slim.rb
@@ -1,1 +1,20 @@
-require 'jekyll-slim'
+require 'slim'
+
+module Jekyll
+  class SlimConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /slim/i
+    end
+
+    def output_ext(ext)
+      '.html'
+    end
+
+    def convert(content)
+      Slim::Template.new(@config['slim']) { content }.render
+    end
+  end
+end
